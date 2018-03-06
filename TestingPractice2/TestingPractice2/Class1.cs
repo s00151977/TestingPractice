bcfb9f00fcdd7b99210b97b3cfc03e1331dbd97e
s00151977 <s00151977@mail.itsligo.ie>
2018-03-06 20:28:08 +0000

    Working nuinit and Fitnesse

--- TestingPractice2/TestingPractice2/Class1.cs
+++ TestingPractice2/TestingPractice2/Class1.cs
@@ -1,13 +1,104 @@
-﻿using System;
+﻿using NUnit.Framework;
+using NUnit;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace TestingPractice2
 {
-    public class Class1
+   /* [TestFixture]
+    static public class Class1
     {
-        //dasfgdg
-    }
+          static Premium p;
+
+          [SetUp]
+          static public void init()
+          {
+              p = new Premium();
+          }
+
+          [Test]
+          static public void Test1()
+          {
+              double expectedResult = 5;
+
+              double actualResult = p.CalcPremium(25, "female");
+
+              Assert.AreEqual(expectedResult, actualResult);
+          }
+
+          [Test]
+          static public void Test2()
+          {
+              double expectedResult = 3.5;
+
+              double actualResult = p.CalcPremium(32, "female");
+
+              Assert.AreEqual(expectedResult, actualResult);
+          }
+
+          [Test] 
+          static public void Test3()
+          {
+              double expectedResult = 1.75;
+
+              double actualResult = p.CalcPremium(55, "female");
+
+              Assert.AreEqual(expectedResult, actualResult);
+          }
+
+          [Test]
+          static public void Test4()
+          {
+              double expectedResult = 0;
+
+              double actualResult = p.CalcPremium(15, "female");
+
+              Assert.AreEqual(expectedResult, actualResult);
+          }
+
+          [Test]
+          static public void Test5()
+          {
+              double expectedResult = 6;
+
+              double actualResult = p.CalcPremium(25, "male");
+
+              Assert.AreEqual(expectedResult, actualResult);
+          }
+
+          [Test]
+          static public void Test6()
+          {
+              double expectedResult = 5;
+
+              double actualResult = p.CalcPremium(37, "male");
+
+              Assert.AreEqual(expectedResult, actualResult);
+          }
+
+          [Test]
+          static public void Test7()
+          {
+              double expectedResult = 2.5;
+
+              double actualResult = p.CalcPremium(55, "male");
+
+              Assert.AreEqual(expectedResult, actualResult);
+          }
+
+          [Test]
+          static public void Test8()
+          {
+              double expectedResult = 0;
+
+              double actualResult = p.CalcPremium(15, "male");
+
+              Assert.AreEqual(expectedResult, actualResult);
+          }
+     
+    }*/
 }
