846d5c3459c2b8203a880d0c49a1b1b881ac543f
s00151977 <s00151977@mail.itsligo.ie>
2018-03-06 20:04:16 +0000

    tyju

---
+++ TestingPractice2/TestingPractice2/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingPractice2
+{
+    public class Class1
+    {
+        //dasfgdg
+    }
+}
